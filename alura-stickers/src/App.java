527ccab2d6c340b1fb49dbc4b309a9803ee4dfd2
atk <atkieling@gmail.com.br>
2022-07-21 18:26:51 -0300

    Aula 02 Gerando figurinhas para WhatsApp

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -15,33 +17,45 @@ public class App {
 		URI endereco = URI.create(url);
 
 		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
-		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 
-		String body = response.body();
-		// System.out.println(body);
+        String body = response.body();
 
-		// Extrair só os dados que interessam (titulo, poster, classificação)
-		JsonParser parser = new JsonParser();
-		List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        //System.out.println(body);
 
-		// Exibir e manipular os dados
-		for (Map<String, String> filme : listaDeFilmes) {
-			System.out.print("\u001b[48;2;42;122;228m");
-			System.out.print("\u001b[38;2;255;255;255m");
-			System.out.println(filme.get("title"));
-			System.out.print("\u001b[m");
+        // Extrair só os dados que interessam (titulo, poster, classificação)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
-			String imdbRating = filme.get("imDbRating");
-			Double imdbRatingDouble = Double.parseDouble(imdbRating);
-			long roundRating = Math.round(imdbRatingDouble);
-			System.out.println(roundRating);
-			System.out.println(filme.get("image"));
-			System.out.println(filme.get("imDbRating"));
+        // Exibir e manipular os dados
+        for (Map<String, String> filme : listaDeFilmes) {
 
-			for (int i = 0; i < roundRating; i++) {
-				System.out.print("\u2b50");
-			}
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream InputStream = new URL(urlImagem).openStream();
+
+            String nomeArquivo = titulo + ".png";
+
+            var geradora = new GeradoraDeFigurinhas();
+            geradora.cria(InputStream, nomeArquivo);
+
+            System.out.print("\u001b[48;2;42;122;228m");
+            System.out.print("\u001b[38;2;255;255;255m");
+            System.out.println("Título: " + titulo);
+            System.out.print("\u001b[m");
+
+            String imdbRating = filme.get("imDbRating");
+            Double imdbRatingDouble = Double.parseDouble(imdbRating);
+            long roundRating = Math.round(imdbRatingDouble);
+            //System.out.println(roundRating);
+            System.out.println("Pôster: " + filme.get("image"));
+            System.out.println("Classificação: " + filme.get("imDbRating"));
+
+            for (int i = 0; i < roundRating; i++) {
+                System.out.print("\u2b50");
+            }
 
 			System.out.println();
 		}
---
+++ alura-stickers/src/GeradoraDeFigurinhas.java
@@ -0,0 +1,48 @@
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.Font;
+import java.io.File;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public class GeradoraDeFigurinhas {
+
+	public void cria(InputStream InputStream, String nomeArquivo) throws Exception {
+
+        // Leitura da imagem
+        //InputStream InputStream = new FileInputStream(new File("imagemEntrada/filme.jpg"));
+        //InputStream InputStream = new URL("https://imersao-java-apis.s3.amazonaws.com/TopMovies_7.jpg").openStream();
+		BufferedImage imagemOriginal = ImageIO.read(InputStream);
+
+		// Cria nova imagem em memória com transparência e com tamanho novo
+		int largura = imagemOriginal.getWidth();
+		int altura = imagemOriginal.getHeight();
+		int novaAltura = altura + 200;
+		BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+
+		// Copiar a imagem original para nova imagem (em memória)
+		Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+		graphics.drawImage(imagemOriginal, 0, 0, null);
+
+		// Configurar a fonte
+		var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+		graphics.setColor(Color.YELLOW);
+		graphics.setFont(fonte);
+
+		// Escrever uma frase na nova imagem
+		int framingRectangle = (int) graphics.getFontMetrics().getStringBounds("TOP!", graphics).getWidth();
+		int x = novaImagem.getWidth() / 2 - framingRectangle / 2;
+		int y = novaImagem.getHeight() - 78;
+		graphics.drawString("TOP!", x, y);
+
+		// Escrever a nova imagem em um arquivo
+		ImageIO.write(novaImagem, "png", new File("imagemSaida/" + nomeArquivo));
+	}
+
+    //public static void main(String[] args) throws Exception {
+    //var geradora = new GeradoraDeFigurinhas();
+    //geradora.cria();
+    //}
+}
